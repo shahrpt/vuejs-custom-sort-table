67359310028f8c9ea97b91495baa9c5711903192
Sulman <shahsuliman@gmail.com>
2022-02-01 21:39:22 +0500

    added a table for sorting

---
+++ vuejs-custom-sort-table/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <sortable />
+  </div>
+</template>
+
+<script>
+import sortable from './components/sortable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    sortable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
